5676c1d36d1bdef623ed544954a6ce48cad53c18
Benji Nguyen <benjamin.van.nguyen@gmail.com>
2022-01-04 21:08:19 -0800

    boilerplate projects

---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="font-bold">Hello world</h1>
+    </div>
+  )
+}
---
+++ app/index/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Navbar from "@components/navbar";
+
+export default () => {
+  return (
+    <div className="inline">
+      <Navbar />
+    </div>
+  )
+}
